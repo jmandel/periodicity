99ad400a0df546b0789715e6c04b3b7a31a479ae
Josh Mandel <jmandel@med.harvard.edu>
2026-06-25 10:18:38 -0500

    Use explicit IG Publisher control file

--- _genonce.bat
+++ _genonce.bat
@@ -5,4 +5,5 @@ if not exist input-cache\publisher.jar (
   echo input-cache\publisher.jar is missing. Run _updatePublisher.sh in a Unix-like shell or download publisher.jar manually.
   exit /b 1
 )
-java -jar input-cache\publisher.jar -ig .
+if "%IG_CONTROL%"=="" set IG_CONTROL=ig-gh-actions.ini
+java -jar input-cache\publisher.jar -ig "%IG_CONTROL%" %*
--- _genonce.sh
+++ _genonce.sh
@@ -6,4 +6,5 @@ if [[ ! -f input-cache/publisher.jar ]]; then
   echo "input-cache/publisher.jar is missing; run ./_updatePublisher.sh first" >&2
   exit 1
 fi
-java -jar input-cache/publisher.jar -ig . "$@"
+IG_CONTROL="${IG_CONTROL:-ig-gh-actions.ini}"
+java -jar input-cache/publisher.jar -ig "$IG_CONTROL" "$@"
